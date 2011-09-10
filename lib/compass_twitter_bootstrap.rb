ec32c098135b63f3e44e7fd9793788adf06d0daf
ascrazy <i.alexandr.sh@gmail.com>
2011-09-10 10:58:06 +0300

    Edited lib/compass_twitter_bootstrap.rb via GitHub

--- lib/compass_twitter_bootstrap.rb
+++ lib/compass_twitter_bootstrap.rb
@@ -1,6 +1,6 @@
 require "compass_twitter_bootstrap/version"
 require 'compass'
-Compass::Frameworks.register("afadf", :path => "#{File.dirname(__FILE__)}/..")
+Compass::Frameworks.register("twitter_bootstrap", :path => "#{File.dirname(__FILE__)}/..")
 
 module CompassTwitterBootstrap
   STYLESHEETS = File.expand_path("../stylesheets", __FILE__)
